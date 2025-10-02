c6579d65590124ef5562cfdf7ae3affbc84168d5
Desarrollo <Desarrollo@192.168.0.48>
2025-10-02 09:57:32 +0200

    Configuración de acceso datos con driver

--- bases/src/com/ipartek/formacion/citas/accesodatos/Globales.java
+++ bases/src/com/ipartek/formacion/citas/accesodatos/Globales.java
@@ -1,7 +1,29 @@
 package com.ipartek.formacion.citas.accesodatos;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 class Globales {
-	static final String URL = "jdbc:sqlite:bdd/citas.db?foreign_keys=on";
-	static final String USER = "";
-	static final String PASS = "";
+	static String URL = "jdbc:sqlite:bdd/citas.db?foreign_keys=on";
+	static String USER = "";
+	static String PASS = "";
+	
+	static {
+		try {
+			Properties props = new Properties();
+			
+			props.load(new FileReader("fabrica.properties"));
+			
+			URL = props.getProperty("dao.url");
+			USER = props.getProperty("dao.user");
+			PASS = props.getProperty("dao.pass");
+			
+			String driver = props.getProperty("dao.driver");
+			
+			Class.forName(driver);
+		} catch (ClassNotFoundException | IOException e) {
+			throw new IllegalStateException("No se ha podido leer la configuración", e);
+		}
+	}
 }
