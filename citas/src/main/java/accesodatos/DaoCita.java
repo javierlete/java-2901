bd71601513674c8ffc14f0c83321691523d6f237
Desarrollo <Desarrollo@192.168.0.54>
2025-10-06 10:40:16 +0200

    Acceso a datos

---
+++ citas/src/main/java/accesodatos/DaoCita.java
@@ -0,0 +1,8 @@
+package accesodatos;
+
+import bibliotecas.accesodatos.Dao;
+import modelos.Cita;
+
+public interface DaoCita extends Dao<Cita> {
+	
+}
---
+++ citas/src/main/java/accesodatos/DaoCitaSqlite.java
@@ -0,0 +1,43 @@
+package accesodatos;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+
+import bibliotecas.accesodatos.DaoJdbc;
+import modelos.Cita;
+
+public class DaoCitaSqlite implements DaoCita {
+	private DaoJdbc<Cita> dao = new DaoJdbc<>("jdbc:sqlite:bdd/citas.db", "", "", "org.sqlite.JDBC");
+
+	@Override
+	public Iterable<Cita> obtenerTodos() {
+		return dao.ejecutarConsulta("SELECT * FROM citas", DaoCitaSqlite::mapeador);
+	}
+
+	@Override
+	public Optional<Cita> obtenerPorId(Long id) {
+		return dao.ejecutarConsultaUno("SELECT * FROM citas WHERE id=?", DaoCitaSqlite::mapeador, id);
+	}
+
+	@Override
+	public Cita insertar(Cita cita) {
+		dao.ejecutarCambio("INSERT INTO citas (texto, inicio, fin) VALUES (?,?,?)", cita.getTexto(), cita.getInicio(), cita.getFin());
+		return cita;
+	}
+
+	@Override
+	public Cita modificar(Cita cita) {
+		dao.ejecutarCambio("UPDATE citas SET texto=?, inicio=?, fin=? WHERE id=?", cita.getTexto(), cita.getInicio(), cita.getFin(), cita.getId());
+		return cita;
+	}
+
+	@Override
+	public void borrar(Long id) {
+		dao.ejecutarCambio("DELETE FROM citas WHERE id=?", id);
+	}
+	
+	private static Cita mapeador(ResultSet rs) throws SQLException {
+		return new Cita(rs.getLong("id"), rs.getString("texto"), rs.getTimestamp("inicio").toLocalDateTime(), rs.getTimestamp("fin").toLocalDateTime());
+	}
+}
