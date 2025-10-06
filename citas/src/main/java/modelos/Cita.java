fa70cb04c8c0cfdeea6584bfbc8e6ada4b618438
Desarrollo <Desarrollo@192.168.0.54>
2025-10-06 10:40:08 +0200

    Modelos

---
+++ citas/src/main/java/modelos/Cita.java
@@ -0,0 +1,75 @@
+package modelos;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Cita {
+	private Long id;
+	private String texto;
+	private LocalDateTime inicio;
+	private LocalDateTime fin;
+
+	public Cita(Long id, String texto, LocalDateTime inicio, LocalDateTime fin) {
+		super();
+		this.id = id;
+		this.texto = texto;
+		this.inicio = inicio;
+		this.fin = fin;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDateTime getInicio() {
+		return inicio;
+	}
+
+	public void setInicio(LocalDateTime inicio) {
+		this.inicio = inicio;
+	}
+
+	public LocalDateTime getFin() {
+		return fin;
+	}
+
+	public void setFin(LocalDateTime fin) {
+		this.fin = fin;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(fin, id, inicio, texto);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cita other = (Cita) obj;
+		return Objects.equals(fin, other.fin) && Objects.equals(id, other.id) && Objects.equals(inicio, other.inicio)
+				&& Objects.equals(texto, other.texto);
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Cita [id=%s, texto=%s, inicio=%s, fin=%s]", id, texto, inicio, fin);
+	}
+
+}
