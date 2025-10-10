89c4f83df87d0b1d584f444f2eda0f5f0ea3fc64
Desarrollo <Desarrollo@192.168.0.54>
2025-10-10 08:34:52 +0200

    Acceso protegido a administración sólo para ADMINISTRADORes

---
+++ citas/src/main/java/filtros/AdminFilter.java
@@ -0,0 +1,38 @@
+package filtros;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import modelos.Usuario;
+
+@WebFilter("/cf/admin/*")
+public class AdminFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+
+		HttpSession session = req.getSession();
+
+		Usuario usuario = (Usuario) session.getAttribute("usuario");
+
+		if (usuario != null && usuario.getRol().getNombre().equals("ADMINISTRADOR")) {
+			chain.doFilter(request, response);
+			return;
+		}
+		
+		res.sendRedirect(req.getContextPath() + "/cf/login");
+	}
+
+}
