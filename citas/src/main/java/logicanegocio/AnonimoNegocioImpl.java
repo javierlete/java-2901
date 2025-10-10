d024c5ef8ecbdd72bf883a31fbe63420b38a01e5
Desarrollo <Desarrollo@192.168.0.54>
2025-10-10 09:29:49 +0200

    Logueo de proceso de autenticación

--- citas/src/main/java/logicanegocio/AnonimoNegocioImpl.java
+++ citas/src/main/java/logicanegocio/AnonimoNegocioImpl.java
@@ -1,19 +1,35 @@
 package logicanegocio;
 
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import accesodatos.DaoUsuario;
 import bibliotecas.Fabrica;
 import modelos.Usuario;
 
 public class AnonimoNegocioImpl implements AnonimoNegocio {
+	private static final Logger log = Logger.getLogger(AnonimoNegocioImpl.class.getName());
+	
 	private static final DaoUsuario DAO_USUARIO = (DaoUsuario) Fabrica.obtenerObjeto("dao.usuario");
 	
 	@Override
 	public Optional<Usuario> autenticar(String email, String password) {
 		Optional<Usuario> usuario = DAO_USUARIO.obtenerPorEmail(email);
 
-		if (usuario.isPresent() && usuario.get().getPassword().equals(password)) {
+		if(usuario.isEmpty()) {
+			log.warning("USUARIO NO ENCONTRADO");
+			return Optional.empty();
+		}
+		
+		boolean passwordCorrecta = usuario.get().getPassword().equals(password);
+
+		if(!passwordCorrecta) {
+			log.log(Level.WARNING, "PASSWORD INCORRECTA para el usuario {0}", usuario.get().getEmail());
+		}
+		
+		if (usuario.isPresent() && passwordCorrecta) {
+			log.log(Level.FINEST,"Usuario logueado correctamente: {0}", usuario.get().getEmail());
 			return usuario;
 		}
 
