9ad6d1bf1f5e04b7a337b0615152488f9785e530
Desarrollo <Desarrollo@192.168.0.43>
2025-10-15 10:48:41 +0200

    DaoJpa mejorado con todo el CRUD preparado por defecto

--- citas/src/main/java/bibliotecas/accesodatos/DaoJpa.java
+++ citas/src/main/java/bibliotecas/accesodatos/DaoJpa.java
@@ -1,27 +1,63 @@
 package bibliotecas.accesodatos;
 
-import java.util.function.BiFunction;
+import java.util.Optional;
+import java.util.function.Function;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
-public class DaoJpa {
+public class DaoJpa<T> {
 	private static final EntityManagerFactory factory = Persistence.createEntityManagerFactory("modelos");
 
-	public static <R> R ejecutarJpa(BiFunction<EntityManager, Object[], R> codigo, Object... args) {
+	private final Class<T> clase;
+
+	public DaoJpa(Class<T> clase) {
+		this.clase = clase;
+	}
+
+	public static <R> R ejecutarJpa(Function<EntityManager, R> codigo) {
 		EntityManager em = factory.createEntityManager();
 		EntityTransaction t = em.getTransaction();
 
 		t.begin();
 
-		var resultado = codigo.apply(em, args);
+		var resultado = codigo.apply(em);
 
 		t.commit();
 
 		em.close();
 
 		return resultado;
+	}
+
+	public Iterable<T> obtenerTodos() {
+		return ejecutarJpa(em -> em.createQuery("from " + clase.getName(), clase).getResultList());
+	}
+
+	public Optional<T> obtenerPorId(Long id) {
+		return ejecutarJpa(em -> Optional.ofNullable(em.find(clase, id)));
+	}
+
+	public T insertar(T objeto) {
+		return ejecutarJpa(em -> {
+			em.persist(objeto);
+			return objeto;
+		});
+	}
+
+	public T modificar(T objeto) {
+		return ejecutarJpa(em -> {
+			em.merge(objeto);
+			return objeto;
+		});
+	}
+
+	public void borrar(Long id) {
+		ejecutarJpa(em -> {
+			em.remove(em.find(clase, id));
+			return null;
+		});
 	}
 }
