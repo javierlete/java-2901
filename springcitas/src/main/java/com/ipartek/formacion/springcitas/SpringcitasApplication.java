e951cd6f65c0142363c556979f2ccc0c16b12241
Desarrollo <Desarrollo@192.168.0.53>
2025-10-20 09:35:01 +0200

    Spring Boot/Spring Starter Project/ Lombok, Validation, Spring Data JPA
    Maven, Java, 21, WAR

---
+++ springcitas/src/main/java/com/ipartek/formacion/springcitas/SpringcitasApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.springcitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcitasApplication.class, args);
+	}
+
+}
---
+++ springcitas/src/test/java/com/ipartek/formacion/springcitas/SpringcitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.springcitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
