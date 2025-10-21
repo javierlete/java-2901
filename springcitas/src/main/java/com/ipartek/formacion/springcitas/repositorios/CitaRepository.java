a91d099bba6c91ef14efd39f5a43ebccb524ca6d
Desarrollo <Desarrollo@192.168.0.53>
2025-10-21 10:54:26 +0200

    Configuramos la ruta de cada elemento y añadimos paginación a Citas

--- springcitas/src/main/java/com/ipartek/formacion/springcitas/repositorios/CitaRepository.java
+++ springcitas/src/main/java/com/ipartek/formacion/springcitas/repositorios/CitaRepository.java
@@ -1,9 +1,16 @@
 package com.ipartek.formacion.springcitas.repositorios;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.ipartek.formacion.springcitas.entidades.Cita;
 
-public interface CitaRepository extends CrudRepository<Cita, Long> {
-
+@RepositoryRestResource(collectionResourceRel = "citas", path = "citas")
+public interface CitaRepository extends CrudRepository<Cita, Long>, PagingAndSortingRepository<Cita, Long> {
+	Page<Cita> findByInicioBetween(Pageable pageable, LocalDateTime inicio, LocalDateTime fin);
 }
--- springcitas/src/main/java/com/ipartek/formacion/springcitas/repositorios/RolRepository.java
+++ springcitas/src/main/java/com/ipartek/formacion/springcitas/repositorios/RolRepository.java
@@ -1,9 +1,11 @@
 package com.ipartek.formacion.springcitas.repositorios;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.ipartek.formacion.springcitas.entidades.Rol;
 
+@RepositoryRestResource(collectionResourceRel = "roles", path = "roles")
 public interface RolRepository extends CrudRepository<Rol, Long> {
 
 }
