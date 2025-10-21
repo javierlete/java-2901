09050753dbed5f4ed28b8f1dc012f0859b3a3099
Desarrollo <Desarrollo@192.168.0.53>
2025-10-21 10:56:55 +0200

    Servicio REST hecho a medida con autenticar

---
+++ springcitas/src/main/java/com/ipartek/formacion/springcitas/rest/AnonimoRestController.java
@@ -0,0 +1,31 @@
+package com.ipartek.formacion.springcitas.rest;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.ipartek.formacion.springcitas.entidades.Usuario;
+import com.ipartek.formacion.springcitas.servicios.AnonimoService;
+
+@RestController
+@RequestMapping("/api/v1")
+public class AnonimoRestController {
+	@Autowired
+	private AnonimoService anonimoService;
+	
+	@GetMapping("/autenticar")
+	public Usuario autenticar(String email, String password) {
+		Optional<Usuario> usuario = anonimoService.autenticar(email, password);
+		
+		if(usuario.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+		
+		return usuario.get();
+	}
+}
