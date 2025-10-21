8f477ddf772a82bf2cb4c20c65d09e3ee4ea0bd1
Desarrollo <Desarrollo@192.168.0.53>
2025-10-21 10:55:17 +0200

    Petición de datos relacionados de Rol con join

--- springcitas/src/main/java/com/ipartek/formacion/springcitas/repositorios/UsuarioRepository.java
+++ springcitas/src/main/java/com/ipartek/formacion/springcitas/repositorios/UsuarioRepository.java
@@ -2,10 +2,22 @@ package com.ipartek.formacion.springcitas.repositorios;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.ipartek.formacion.springcitas.entidades.Usuario;
 
+@RepositoryRestResource(collectionResourceRel = "usuarios", path = "usuarios")
 public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
 	Optional<Usuario> findByEmail(String email);
+
+	@EntityGraph(attributePaths = "rol")
+	Iterable<Usuario> findAll();
+	
+//	@Query("select new com.ipartek.formacion.springcitas.dtos.UsuarioRolDto(u.id, u.nombre, u.email, r.id, r.nombre) from Usuario u left join u.rol r")
+//	List<UsuarioRolDto> usuariosConRoles();
+	
+//	@Query("from Usuario u left join fetch u.rol r")
+//	Iterable<Usuario> usuariosConRoles();
 }
