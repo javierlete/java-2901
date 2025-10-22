761b9e4683ba7409f75ce9c94e90ca4db7a50dd9
Desarrollo <Desarrollo@192.168.0.49>
2025-10-22 10:09:59 +0200

    Cambiamos a Collection en UsuarioRepository para poder usar stream()

--- springcitas/src/main/java/com/ipartek/formacion/springcitas/repositorios/UsuarioRepository.java
+++ springcitas/src/main/java/com/ipartek/formacion/springcitas/repositorios/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package com.ipartek.formacion.springcitas.repositorios;
 
+import java.util.Collection;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.EntityGraph;
@@ -13,7 +14,7 @@ public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
 	Optional<Usuario> findByEmail(String email);
 
 	@EntityGraph(attributePaths = "rol")
-	Iterable<Usuario> findAll();
+	Collection<Usuario> findAll();
 	
 //	@Query("select new com.ipartek.formacion.springcitas.dtos.UsuarioRolDto(u.id, u.nombre, u.email, r.id, r.nombre) from Usuario u left join u.rol r")
 //	List<UsuarioRolDto> usuariosConRoles();
